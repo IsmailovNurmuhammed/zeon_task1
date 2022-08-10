19544c3d16e35bf2ee2dcbf82c1e0f579926cac4
Nurmuhammed <ismailovnurmuhammed@gmail.com>
2022-08-10 14:50:20 +0600

    pre release

--- js/burger.js
+++ js/burger.js
@@ -1,13 +1,21 @@
-const burger = document.getElementById("mobile-burger");
-let menu = document.querySelector(".nav");
+const burger = document.querySelector("#mobile-burger");
+const menu = document.querySelector(".nav");
 const outside = document.querySelector("body");
+const navLinks = document.querySelectorAll(".nav__link");
 
 burger.addEventListener("click", function () {
-    if (burger.classList.contains("active")) {
-        outside.style.overflow = "visible";
-    } else {
-        outside.style.overflow = "hidden";
-    }
+    burger.classList.contains("active")
+        ? (outside.style.overflow = "visible")
+        : (outside.style.overflow = "hidden");
     burger.classList.toggle("active");
     menu.classList.toggle("active");
 });
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function () {
+        burger.classList.contains("active")
+            ? (outside.style.overflow = "visible")
+            : (outside.style.overflow = "hidden");
+        burger.classList.toggle("active");
+        menu.classList.toggle("active");
+    });
+}
